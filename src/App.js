fe648c1ab8308a961a61123e4e12aecf29dde656
Your Name <you@example.com>
2024-01-17 12:50:07 +0300

    mjjj

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const Header = ({ logo, title }) => (
+  <div className='App-header'>
+    <img src={logo} alt="logo" className='App-logo'/>
+    <h1>{title}</h1>
+  </div>
+);
+
+const Nav = ({ menuItems }) => (
+  <ul>
+    {menuItems.map((item, index) => (
+      <li key={index}>
+        <a href={item.link}>{item.title}</a>
+      </li>
+    ))}
+  </ul>
+);
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>@copyright все права защищены {currentYear} год.</p>
+    </div>
+  );
+};
+
+const Title = ({ text, size, color }) =>{
+  const styles = {
+    fontSize: size,
+    color: color || 'black'
+  };
+
+  return <h1 style={styles}>{text}</h1>;
+}
+
+const SimpleInput = ({ type, placeholder, value, onChange }) => {
+  return <input type={type} placeholder={placeholder} value={value} onChange={onChange} />;
+};
+
+function App() {
+  const [isShow, setIsShow] = useState(false);
+  const menuItems = [
+    { title: 'Новости', link: '/news' },
+    { title: 'Блог', link: '/' },
+    { title: 'События', link: '/' },
+    { title: 'Погода', link: '/' },
+    { title: 'Расписание', link: '/' },
+  ];
+  const logoUrl = 'https://sun9-9.userapi.com/impg/Hv2wcZW8kd5fuqftg6J6DLqt8QYOki-_hc3PHg/mB79cUmTxE0.jpg?size=706x706&quality=96&sign=3b10c03535aff19ca0f8e490fee10814&c_uniq_tag=eyA7MEe4UPmmqDOcIr8vNj-5Nhs7_E6ykAKpn92ReNQ&type=album';
+  const siteTitle = 'Название';
+  const handleInputChange = (event) => {
+    
+  };
+
+  const [isVisible, setIsVisible] = useState(true);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+
+  return (
+    <div className='App'>
+      <Header logo={logoUrl} title={siteTitle} />
+      <Nav menuItems={menuItems} />
+      <Title text="Title" size="24px" color="blue" />
+      <SimpleInput type="text" placeholder="Введите" value="" onChange={handleInputChange} />
+
+      <div>
+        <button onClick={toggleVisibility}>{isVisible ? 'Скрыть' : 'Показать'}</button>
+        {isVisible && <p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed pharetra metus, a fermentum leo. Duis pharetra dui sem, at sollicitudin nisl sodales eget. Curabitur imperdiet dolor diam, et rutrum est venenatis a. Pellentesque rhoncus porta est in pellentesque. Sed non accumsan turpis. Phasellus lobortis pulvinar tristique. Proin sit amet ex eu mi molestie imperdiet vitae id dui. Ut ligula nunc, efficitur in pellentesque nec, semper quis urna.</p>}
+      </div>
+
+      <Footer />
     </div>
   );
 }
